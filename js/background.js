cde0b840fa64fb3604fd7d3d4c23408dc136e064
m1n720n9 <skmnjng@gmail.com>
2022-05-27 23:37:37 +0900

    Add bg_img & css greeting

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ["bg-1.jpg", "bg-2.jpg", "bg-3.jpg"];
+const images = ["bg-1.jpg","bg-2.jpg","bg-3.jpg","bg-4.jpg","bg-5.jpg","bg-6.jpg"];
 
 const chosenImage = images[Math.floor(Math.random()*images.length)];
 
